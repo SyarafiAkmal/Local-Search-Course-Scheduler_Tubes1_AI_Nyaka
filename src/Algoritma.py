4f10df0ca1c7b087a1bae1e42600a5ac851733b0
Akmal2205 <syarafiakml2004@gmail.com>
2025-10-01 12:58:08 +0700

    add: konversi input -> state awal random saat instansiasi kelas algoritma
    Note: input di bagian kelas_mata_kuliah tambah properti random_seed buat reproducibility

--- src/Algoritma.py
+++ src/Algoritma.py
@@ -1,40 +1,82 @@
+from Kelas import Kelas
+
 class ParentAlgorithm:
-    def __init__(self, state, mahasiswa):
+    def __init__(self, input):
         """
-        Inisialisasi kelas parent algoritma dengan state awal dan data mahasiswa.
-        state: list of Kelas
+        Inisialisasi kelas parent algoritma dengan state awal dan data mahasiswa dan juga inisialisasi state awal
+        state: list of Kelas    
         mahasiswa: list of dict -> {"nim": str, "daftar_mk": list of str, "prioritas": list of int}
         NOTE: prioritas posisinya respective dengan daftar_mk
         """
-        self.state = state
-        self.mahasiswa = mahasiswa
+        if not input['mahasiswa']:
+            raise ValueError("Input tidak valid: mahasiswa kosong")
+        
+        self.mahasiswa = input['mahasiswa']
+        # self.dosen = input['dosen'] buat bonus
+        self.convert_input_to_state(input)
 
     def tukar_jadwal(self):
-        # TODO: implement swap schedule logic
+        # TODO: implement swap schedule logic, masih nunggu revisi spek
         pass
 
     def pindah_jadwal(self):
-        # TODO: implement move schedule logic
+        # TODO: implement move schedule logic, masih nunggu revisi spek
         pass
 
     def fungsi_objektif(self):
-        # TODO: implement objective function logic
+        # TODO: implement objective function logic, masih nunggu revisi spek
+        # BONUS: tambah komponen jadwal dosen, bentuk input jadwal dosen bebas (harusnya bikin variabel baru kayak mahasiswa)
         pass
 
+    def convert_input_to_state(self, input):
+        import random
+        if not input['kelas_mata_kuliah'] or not input['ruangan']:
+            raise ValueError("Input tidak valid: kelas_mata_kuliah atau ruangan kosong")
+        
+        self.state = []
+
+        for i in range(len(input['kelas_mata_kuliah'])):
+            if input['kelas_mata_kuliah'][i]['sks'] > 2:
+                kelas_sesi_1 = Kelas(
+                    mata_kuliah={
+                        "kode": input['kelas_mata_kuliah'][i]['kode'] + "_S1",
+                        "jumlah_mhs": input['kelas_mata_kuliah'][i]['jumlah_mhs'] // 2,
+                        "sks": 2,
+                        "random_seed": input['kelas_mata_kuliah'][i]['random_seed']
+                    },
+                    ruangan=input['ruangan'][random.randint(0, len(input['ruangan'])-1)]
+                )
+                kelas_sesi_2 = Kelas(
+                    mata_kuliah={
+                        "kode": input['kelas_mata_kuliah'][i]['kode'] + "_S2",
+                        "jumlah_mhs": input['kelas_mata_kuliah'][i]['jumlah_mhs'] - (input['kelas_mata_kuliah'][i]['jumlah_mhs'] // 2),
+                        "sks": input['kelas_mata_kuliah'][i]['sks'] - 2,
+                        "random_seed": input['kelas_mata_kuliah'][i]['random_seed'] + 1
+                    },
+                    ruangan=input['ruangan'][random.randint(0, len(input['ruangan'])-1)]
+                )
+                self.state.append(kelas_sesi_1)
+                self.state.append(kelas_sesi_2)
+            else:
+                kelas = Kelas(
+                    mata_kuliah=input['kelas_mata_kuliah'][i], 
+                    ruangan=input['ruangan'][random.randint(0, len(input['ruangan'])-1)]
+                )
+                self.state.append(kelas)
+
+    def show_state(self):
+        for kelas in self.state:
+            kelas.get_info()
+            print("\n")
+
 class HC_SA(ParentAlgorithm):
-    def __init__(self, state, mahasiswa):
-        super().__init__(state)
-        self.state = state
-        self.mahasiswa = mahasiswa
+    def __init__(self, input):
+        super().__init__(input)
 
 class SimulatedAnnealing(ParentAlgorithm):
-    def __init__(self, state, mahasiswa):
-        super().__init__(state)
-        self.state = state
-        self.mahasiswa = mahasiswa
+    def __init__(self, input):
+        super().__init__(input)
 
 class GeneticAlgorithm(ParentAlgorithm):
-    def __init__(self, state, mahasiswa):
-        super().__init__(state)
-        self.state = state
-        self.mahasiswa = mahasiswa
+    def __init__(self, input):
+        super().__init__(input)
--- src/Kelas.py
+++ src/Kelas.py
@@ -4,6 +4,7 @@ class Kelas:
         Inisialisasi kelas dengan mata kuliah dan ruangan.
         mata_kuliah: dict -> {"kode": str, jumlah_mhs: int, sks: int}
         ruangan: dict -> {"kode": str, kuota: int}
+        jadwal: dict -> {"jadwal_mulai": [hari: str, jam: int], "jadwal_selesai": [hari: str, jam: int]}
         """
         self.mata_kuliah = mata_kuliah
         self.ruangan = ruangan
@@ -11,9 +12,14 @@ class Kelas:
 
     def randomize_jadwal(self):
         import random
+        random.seed(self.mata_kuliah["random_seed"])
         hari = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat']
-        jam_mulai = random.randint(7, 17)
+        jam_mulai = random.randint(7, 15)
+        hari_random = hari[random.randint(0, 4)]
         self.jadwal = {
-            "jadwal_mulai": [hari[random.randint(0, 4)], jam_mulai],
-            "jadwal_selesai": [hari[random.randint(0, 4)], jam_mulai + self.mata_kuliah["sks"]]
+            "jadwal_mulai": [hari_random, jam_mulai],
+            "jadwal_selesai": [hari_random, jam_mulai + self.mata_kuliah["sks"]]
             }
+        
+    def get_info(self):
+        print(f"Mata Kuliah: {self.mata_kuliah['kode']}\nSKS: {self.mata_kuliah['sks']}\nJumlah Mahasiswa: {self.mata_kuliah['jumlah_mhs']}\nRuangan: {self.ruangan['kode']}\nKuota Ruangan: {self.ruangan['kuota']}\nJadwal Mulai: {self.jadwal['jadwal_mulai'][0]} jam {self.jadwal['jadwal_mulai'][1]}\nJadwal Selesai: {self.jadwal['jadwal_selesai'][0]} jam {self.jadwal['jadwal_selesai'][1]}")
