cecb9dc0139a354005af3cbd9fb9b331bda72a1a
Akmal2205 <syarafiakml2004@gmail.com>
2025-10-01 14:43:12 +0700

    add: logic buat hitung fungsi objektif

--- src/Algoritma.py
+++ src/Algoritma.py
@@ -15,6 +15,12 @@ class ParentAlgorithm:
         # self.dosen = input['dosen'] buat bonus
         self.convert_input_to_state(input)
 
+
+        # debug_kls = self.get_kelas_by_kode("IF3140_K01_S2")
+        # debug_kls[0].jadwal = {"jadwal_mulai": ['Selasa', 9], "jadwal_selesai": ['Selasa', 11]}
+        # debug_kls[0].ruangan = {"kode": "7606", "kuota": 60}
+        # debug_kls[0].get_info()
+
     def tukar_jadwal(self):
         # TODO: implement swap schedule logic, masih nunggu revisi spek
         pass
@@ -23,17 +29,71 @@ class ParentAlgorithm:
         # TODO: implement move schedule logic, masih nunggu revisi spek
         pass
 
-    def fungsi_objektif(self):
-        # TODO: implement objective function logic, masih nunggu revisi spek
+    def hitung_fungsi_objektif(self):
+        # TODO: implement objective function logic
         # BONUS: tambah komponen jadwal dosen, bentuk input jadwal dosen bebas (harusnya bikin variabel baru kayak mahasiswa)
-        pass
+
+        obj_jadwal_kelas = 0
+        obj_kuota_kelas = 0
+        obj_jadwal_mahasiswa = 0
+
+        # Cek jadwal kelas
+        for i in range (len(self.state)):
+            for j in range (i+1, len(self.state)):
+                # print(max(self.state[i].jadwal['jadwal_mulai'][1], self.state[j].jadwal['jadwal_mulai'][1]), min(self.state[i].jadwal['jadwal_selesai'][1], self.state[j].jadwal['jadwal_selesai'][1]), "compare", self.state[i].mata_kuliah['kode'], self.state[j].mata_kuliah['kode'], self.state[i].ruangan['kode'], self.state[j].ruangan['kode'])
+                if self.state[i].jadwal['jadwal_mulai'][0] == self.state[j].jadwal['jadwal_mulai'][0] and max(self.state[i].jadwal['jadwal_mulai'][1], self.state[j].jadwal['jadwal_mulai'][1]) < min(self.state[i].jadwal['jadwal_selesai'][1], self.state[j].jadwal['jadwal_selesai'][1]) and self.state[i].ruangan['kode'] == self.state[j].ruangan['kode']:
+                    # Mata Kuliah Saling Tabrakan Jadwal
+                    print("\nTabrakan")
+                    self.state[i].get_info()
+                    print("\n")
+                    self.state[j].get_info()
+                    obj_jadwal_kelas -= 3
+
+            kuota = self.state[i].ruangan['kuota']
+            jumlah_mhs_daftar = self.state[i].mata_kuliah['jumlah_mhs']
+
+            if jumlah_mhs_daftar > kuota:
+                # Ruangan Kelebihan Kapasitas
+                obj_kuota_kelas -= (jumlah_mhs_daftar - kuota) * 2
+        
+        # Cek jadwal mahasiswa
+        for mhs in self.mahasiswa:
+            daftar_mk = mhs['daftar_mk']
+            kelas = []
+            
+            for mk in daftar_mk:
+                kelas += self.get_kelas_by_kode(mk)
+            
+            for i in range(len(kelas)):
+                for j in range(i+1, len(kelas)):
+                    if kelas[i].jadwal['jadwal_mulai'][0] == kelas[j].jadwal['jadwal_mulai'][0] and max(kelas[i].jadwal['jadwal_mulai'][1], kelas[i].jadwal['jadwal_mulai'][1]) < min(kelas[i].jadwal['jadwal_selesai'][1], kelas[i].jadwal['jadwal_selesai'][1]):
+                        print(f"mhs {mhs['nim']} tabrakan jadwal {kelas[i].mata_kuliah['kode']} dan {kelas[j].mata_kuliah['kode']}")
+                        obj_jadwal_mahasiswa -= 2
+
+        # Debug purposes
+        print("Objektif:", obj_jadwal_mahasiswa + obj_kuota_kelas + obj_jadwal_kelas)
+        print("Objektif Jadwal Kelas:", obj_jadwal_kelas)
+        print("Objektif Kuota Kelas:", obj_kuota_kelas)
+        print("Objektif Jadwal Mahasiswa:", obj_jadwal_mahasiswa)
+
+
+    def get_kelas_by_kode(self, kode):
+        result = []
+        for kelas in self.state:
+            if kode in kelas.mata_kuliah['kode']:
+                result.append(kelas)
+
+        if result:
+            return result
+        
+        return None
 
     def convert_input_to_state(self, input):
         import random
         if not input['kelas_mata_kuliah'] or not input['ruangan']:
             raise ValueError("Input tidak valid: kelas_mata_kuliah atau ruangan kosong")
         
-        self.state = []
+        self.state: list[Kelas] = []
 
         for i in range(len(input['kelas_mata_kuliah'])):
             if input['kelas_mata_kuliah'][i]['sks'] > 2:
@@ -63,6 +123,7 @@ class ParentAlgorithm:
                     ruangan=input['ruangan'][random.randint(0, len(input['ruangan'])-1)]
                 )
                 self.state.append(kelas)
+
 
     def show_state(self):
         for kelas in self.state:
--- src/main.py
+++ src/main.py
@@ -27,7 +27,7 @@ hill_climbing = HC_SA(
                 "kode": "IF3140_K01",
                 "jumlah_mhs": 55,
                 "sks": 3,
-                "random_seed": 12
+                "random_seed": 46
             },
             {
                 "kode": "IF3110_K02",
@@ -36,7 +36,7 @@ hill_climbing = HC_SA(
                 "random_seed": 19
             },
             {
-                "kode": "IF3140_K01",
+                "kode": "IF3140_K02",
                 "jumlah_mhs": 55,
                 "sks": 3,
                 "random_seed": 27
@@ -81,4 +81,79 @@ hill_climbing = HC_SA(
     }
 )
 
-hill_climbing.show_state()
+hill_climbing_small_sample = HC_SA(
+    input={
+        "kelas_mata_kuliah": [
+            {
+                "kode": "IF3071_K01",
+                "jumlah_mhs": 60,
+                "sks": 4,
+                "random_seed": 46
+            },
+            {
+                "kode": "IF3130_K01",
+                "jumlah_mhs": 45,
+                "sks": 2,
+                "random_seed": 50
+            },
+            {
+                "kode": "IF3110_K02",
+                "jumlah_mhs": 70,
+                "sks": 2,
+                "random_seed": 32
+            },
+            # {
+            #     "kode": "IF3140_K01",
+            #     "jumlah_mhs": 55,
+            #     "sks": 3,
+            #     "random_seed": 46
+            # },
+            # {
+            #     "kode": "IF3110_K02",
+            #     "jumlah_mhs": 70,
+            #     "sks": 2,
+            #     "random_seed": 19
+            # },
+            # {
+            #     "kode": "IF3140_K02",
+            #     "jumlah_mhs": 55,
+            #     "sks": 3,
+            #     "random_seed": 27
+            # }
+        ],
+        "ruangan": [
+            {
+                "kode": "7609",
+                "kuota": 60
+            },
+            {
+                "kode": "7606",
+                "kuota": 80   
+            },
+            {
+                "kode": "multimedia",
+                "kuota": 40   
+            }
+        ],
+        "mahasiswa": [
+            {
+                "nim": "13520001",
+                "daftar_mk": ["IF3071_K01", "IF3130_K01", "IF3110_K02"],
+                "prioritas": [1, 2, 3]
+            },
+            {
+                "nim": "13520002",
+                "daftar_mk": ["IF3071_K01", "IF3110_K02"],
+                "prioritas": [1, 2]
+            },
+            {
+                "nim": "13520003",
+                "daftar_mk": ["IF3130_K01", "IF3071_K01"],
+                "prioritas": [1, 2]
+            },
+        ]
+    }
+)
+
+hill_climbing_small_sample.show_state()
+hill_climbing_small_sample.hitung_fungsi_objektif()
